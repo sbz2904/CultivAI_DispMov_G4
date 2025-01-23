0f190c95a9c9b62c7bde28f5a39ed498c708f622
sbz2904 <hsbaquero@uce.edu.ec>
2025-01-23 14:31:49 -0500

    Implementación de Chatbot, usando Gemini

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import HomeScreen from './src/screens/HomeScreen';
+import ChatScreen from './src/screens/ChatScreen';
 
 export default function App() {
   return <HomeScreen />;
+  return <ChatScreen />;
 }
